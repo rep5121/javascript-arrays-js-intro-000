3b341cbb0c8f8c269dacd8068921a6a87f94dd8c
bold-mixer-0681 <renee.pujol@gmail.com>
2020-08-17 06:00:32 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -24,7 +24,7 @@ function accessElementInArray(array, index){
   }
 
 function destructivelyRemoveElementFromBeginningOfArray(array){
-    return array.shift(0)
+    return array.shift(1)
   }
 
 function removeElementFromBeginningOfArray(array){
